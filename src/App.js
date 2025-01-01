c075128a49616bb2954ccba452b1fd361d8e3af1
RVPSA <akeshramasinghe@gmail.com>
2025-01-01 14:21:22 +0100

    simple UI and triggering methods which are in the grpcClient file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { createGame, joinGame } from "./grpcclient/GrpcClient";
+import "./App.css";
 
 function App() {
+  const [createGameUserName, setCreateGameUserName] = useState("");
+  const [joinGameUserName, setJoinGameUserName] = useState("");
+  const [gameId, setGameId] = useState("");
+
+  const handleCreateGame = async () => {
+    if (createGameUserName.trim()) {
+      try {
+        await createGame(createGameUserName);
+        setCreateGameUserName("");
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
+    }
+  };
+
+  const handleJoinGame = async () => {
+    if (joinGameUserName.trim()) {
+      try {
+        await joinGame(gameId, joinGameUserName);
+        setGameId("");
+        setJoinGameUserName("");
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
+    }
+  };
+
   return (
-    <div>
-      ABC
-    </div>
+    <>
+      <div>
+        <div>
+          <h1>Create Game</h1>
+          <label>
+            User name:{" "}
+            <input
+              type="text"
+              onChange={(e) => {
+                setCreateGameUserName(e.target.value);
+              }}
+              value={createGameUserName}
+            />
+          </label>
+          <button onClick={handleCreateGame}>Create Game</button>
+        </div>
+        <div>
+          <h1>Join Game</h1>
+          <label>
+            User name:{" "}
+            <input
+              type="text"
+              onChange={(e) => {
+                setJoinGameUserName(e.target.value);
+              }}
+              value={joinGameUserName}
+            />
+          </label>
+          <label>
+            Game Id:{" "}
+            <input
+              type="text"
+              onChange={(e) => {
+                setGameId(e.target.value);
+              }}
+              value={gameId}
+            />
+          </label>
+          <button onClick={handleJoinGame}>Join Game</button>
+        </div>
+      </div>
+    </>
   );
 }
 
