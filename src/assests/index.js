d2585e01b843b69c3cfb81829b4dfec1e84270aa
RVPSA <akeshramasinghe@gmail.com>
2025-01-08 21:41:21 +0100

    add new svg to show win and loss status

--- src/assests/index.js
+++ src/assests/index.js
@@ -2,5 +2,7 @@ import tictactoe from "./tictactoe.jpg";
 
 import check from "./check.svg";
 import cross from "./cross.svg";
-import circle from "./circle.svg"
-export { tictactoe, check, cross,circle };
+import circle from "./circle.svg";
+import loss from "./loss.svg";
+import win from "./win.svg";
+export { tictactoe, check, cross,circle,loss,win };
